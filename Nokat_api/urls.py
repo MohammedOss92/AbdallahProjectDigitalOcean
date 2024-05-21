89fafe43a91664eab1e3bad5c782529b42812930
mohammed <abdallahomar1992@gmail.com>
2024-05-21 13:46:23 +0300

    Update urls.py

--- Nokat_api/urls.py
+++ Nokat_api/urls.py
@@ -11,6 +11,7 @@ path('nokatup/<int:pk>', generics_pk_Nokat.as_view(), name='snippets-list'),
 path('add_image_nokat/', views.add_image_nokat, name='add_imgs'),
 path('imgnokatapi/', SnippetsListView.as_view(), name='snippets-list'),
 path('imgnokatup/<int:pk>', generics_pk_imgNokat.as_view(), name='snippets-list'),
+path('imgsNokatapiold/', views.imgsNokatapi, name='add_imgs'),
 
 
 
